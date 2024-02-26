3710aec9afacbef07b054ad49bab9fe7fd67193d
Access547 <86067971+Access547@users.noreply.github.com>
2024-02-26 13:59:27 +0000

    Fixed jittering
    The reason it was jittering wasn't because of overshooting, but because the nav agent still had it's target position set. Using a signal to detect when the taget had been reached, you can set it to nothing, which will stop the movement

--- Scripts/unit.gd
+++ Scripts/unit.gd
@@ -2,16 +2,32 @@ extends CharacterBody2D
 class_name Unit
 
 @onready var navAgent = $NavigationAgent2D
+@onready var animPlayer = $AnimationPlayer
+
+
 var speed: float = 50
 
 func _physics_process(delta):
+	#Movement (Note the unit only moves when it's got a target position to go to
 	if navAgent.target_position:
 		var dir = to_local(navAgent.get_next_path_position()).normalized()
 		velocity = dir * speed
 		move_and_slide()
+	
+		#Animation setting
+		if dir:
+			animPlayer.play("Moving")
+		else:
+			animPlayer.play("Idle")
+	
+	
 
 
-
+#Function that just updates the units target position with a Vector2 position
 func UpdateTargetPosition(position: Vector2):
 	navAgent.target_position = position
-	print(str("Target position set: ", navAgent.target_position))
+
+
+#Signal that emits when target has reached its destination
+func _on_navigation_agent_2d_navigation_finished():
+	navAgent.target_position = Vector2(0,0)
